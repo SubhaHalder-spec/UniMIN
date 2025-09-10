a880574beba02377cda2fb5f9c087896fd1539f5
Subha Halder <133703969+SubhaHalder-spec@users.noreply.github.com>
2025-09-10 10:44:39 +0530

    Update UniMIN.R

--- R/UniMIN.R
+++ R/UniMIN.R
@@ -1,14 +1,15 @@
-#' UniMIN test for testing equality means against umbrella-ordered alternatives (peak = ceiling(k/2) = h) in one-way ANOVA
+#' UniMIN test for testing equality means against umbrella-ordered alternatives (peak = h) in one-way ANOVA
 #' @export
 #' @param sample_data list
 #' @param significance_level numeric
+#' @param h numeric
 #' @return Critical value numeric
 #' @return Test statistic value numeric
 #' @return Result Character
-#' @details Testing of H_0:mu_1 = mu_2 = ... = mu_k vs H_1:mu_1 <=.....<= mu_(h-1)<= mu_h >= mu_(h+1)>=....>= mu_k (at least one strict inequality), where mu_i represents the population means of the i-th treatment. The input consists of two variables: sample_data and significance_level. The output consists of the critical value, the UniMIN test statistic value, and the result, which indicates whether to reject or not reject the null hypothesis.
+#' @details Testing of H_0:mu_1 = mu_2 = ... = mu_k vs H_1:mu_1 <=.....<= mu_(h-1)<= mu_h >= mu_(h+1)>=....>= mu_k (at least one strict inequality), where mu_i represents the population means of the i-th treatment. The input consists of three variables: sample_data, significance_level, and peak = h. The output consists of the critical value, the UniMAX test statistic value, and the result, which indicates whether to reject or not reject the null hypothesis.
 #' @import stats
 #' @author Subha Halder
-UniMIN <- function(sample_data, significance_level){
+UniMIN <- function(sample_data, significance_level, h){
   set.seed(456)
   sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
   num_samples = 100000
@@ -18,14 +19,14 @@ UniMIN <- function(sample_data, significance_level){
   for (i in 1:num_samples) {
     bootstrap_samples <- lapply(sample_data, function(x) rnorm(n = length(x), mean = 0, sd = sqrt(var(x))))
     D_star_min[i] <- min(c(
-      sapply(2:ceiling(num_datasets / 2), function(j) {
+      sapply(2:h, function(j) {
         (mean(bootstrap_samples[[j]]) - mean(bootstrap_samples[[j - 1]])) /
           sqrt(
             (var(bootstrap_samples[[j]]) / length(bootstrap_samples[[j]])) +
               (var(bootstrap_samples[[j - 1]]) / length(bootstrap_samples[[j - 1]]))
           )
       }),
-      sapply((ceiling(num_datasets / 2) + 1):num_datasets, function(j) {
+      sapply((h + 1):num_datasets, function(j) {
         (-mean(bootstrap_samples[[j]]) + mean(bootstrap_samples[[j - 1]])) /
           sqrt(
             (var(bootstrap_samples[[j]]) / length(bootstrap_samples[[j]])) +
@@ -38,14 +39,14 @@ UniMIN <- function(sample_data, significance_level){
   sort_D_star_min <- sort(D_star_min)
   quantile_value <- quantile(sort_D_star_min, probs = 1 - significance_level)
   UMIN <- min(c(
-    sapply(2:ceiling(num_datasets / 2), function(i) {
+    sapply(2:h, function(i) {
       (mean(sample_data[[i]]) - mean(sample_data[[i - 1]])) /
         sqrt(
           (var(sample_data[[i]]) / length(sample_data[[i]])) +
             (var(sample_data[[i - 1]]) / length(sample_data[[i - 1]]))
         )
     }),
-    sapply((ceiling(num_datasets / 2) + 1):num_datasets, function(i) {
+    sapply((h + 1):num_datasets, function(i) {
       (-mean(sample_data[[i]]) + mean(sample_data[[i - 1]])) /
         sqrt(
           (var(sample_data[[i]]) / length(sample_data[[i]])) +
@@ -60,4 +61,5 @@ UniMIN <- function(sample_data, significance_level){
   }
   return(paste("Critical value:", quantile_value, "; UniMIN Test statistic:", UMIN, "; Result:", result))
 }
+
 
